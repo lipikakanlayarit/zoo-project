f6fa134d6c13d9a77c37e9e0ed83b14d5809df45
Lttrumbrla13 <lipikakanlayarit@gmail.com>
2024-11-11 05:32:48 +0000

    change

--- test_zoo.py
+++ test_zoo.py
@@ -7,7 +7,62 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(11), 50)
+    
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(19), 100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(24), 150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(50), 150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1),"Invalid")
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(0),50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(1),50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(11),50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12),50)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13),100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(14),100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(19),100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20),100)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21),150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(22),150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(59),150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60),150)
+
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61),100)
+
+    
     # Add your additional test cases here.
 
 if __name__ == '__main__':
